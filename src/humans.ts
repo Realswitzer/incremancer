c3c7d1cf236355ad85b36bf65f4ce60c15c09420
Realswitzer <switzr@tuta.io>
2026-08-24 10:02:13 -0500

    fix(port:danemancer): More tweaks to human health (a3b99fc)
    Co-authored-by: CirusDane <154769003+CirusDane@users.noreply.github.com>

--- src/humans.ts
+++ src/humans.ts
@@ -218,9 +218,9 @@ export class Humans {
       return 8500000 * Math.pow(1.03, level - 499);
     }
     if (level > 1499) {
-      return 8500000 * Math.pow(1.02, level - 1499);
+      return 498e16 * Math.pow(1.02, level - 1499);
     }
-    return 8500000 * Math.pow(1.025, level - 999);
+    return 21e12 * Math.pow(1.025, level - 999);
   }
 
   getAttackDamage(): void {
