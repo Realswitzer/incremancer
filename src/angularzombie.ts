cde59c33bd04290f594353f6fea78145883e5164
Realswitzer <switzr@tuta.io>
2026-08-25 12:22:21 -0500

    feat(port:danemancer): change skeleton offer scaling (ac11c1d)
    Co-authored-by: CirusDane <154769003+CirusDane@users.noreply.github.com>

--- src/angularzombie.ts
+++ src/angularzombie.ts
@@ -991,14 +991,14 @@ angular
           return (
             skeleton.persistent.skeletons > 0 &&
             zm.model.persistentData.trophies.length >=
-              (skeleton.persistent.xpRate < 17
+              (skeleton.persistent.xpRate < 15
                 ? 20 * skeleton.persistent.xpRate
-                : 80 + (Math.log2(skeleton.persistent.xpRate) - 3) * 80)
+                : 160 + (Math.log2(skeleton.persistent.xpRate) - 3) * 80)
           );
         },
         trophies() {
           return skeleton.persistent.skeletons > 0
-            ? ` - ${zm.model.persistentData.trophies.length} / ${skeleton.persistent.xpRate < 17 ? 20 * skeleton.persistent.xpRate : 80 + (Math.log2(skeleton.persistent.xpRate) - 3) * 80} Trophies`
+            ? ` - ${zm.model.persistentData.trophies.length} / ${skeleton.persistent.xpRate < 15 ? 20 * skeleton.persistent.xpRate : 160 + (Math.log2(skeleton.persistent.xpRate) - 3) * 80} Trophies`
             : "";
         },
         talentPoints: () => skeleton.talentPoints,
