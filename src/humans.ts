70c742c1c6b3cf534c8770d5df7097be0592a569
Realswitzer <switzr@tuta.io>
2026-08-27 10:14:47 -0500

    feat(port:danemancer): change max health scaling (9aed773)
    Co-authored-by: CirusDane <154769003+CirusDane@users.noreply.github.com>

--- src/humans.ts
+++ src/humans.ts
@@ -217,8 +217,11 @@ export class Humans {
     if (level < 999) {
       return 8500000 * Math.pow(1.03, level - 499);
     }
-    if (level > 1499) {
+    if (level < 1499) {
       return 498e16 * Math.pow(1.021, level - 1499);
+    }
+    if (level > 2299) {
+      return 8.45e25 * Math.pow(1.025, level - 2299);
     }
     return 22e12 * Math.pow(1.025, level - 999);
   }
